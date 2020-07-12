98d47684a0447f876d0fc1285bc77bfdcd7bddef
xiaoqiu <310622858@qq.com>
2020-07-12 17:46:13 +0800

    6.新增点击编辑按钮显示input和确认取消按钮

--- src/pages/Edu/Subject/index.jsx
+++ src/pages/Edu/Subject/index.jsx
@@ -2,10 +2,16 @@
 import React, { Component } from "react";
 
 // 引入按钮 表格
-import { Button, Table, Tooltip } from 'antd';
+import { Button, Table, Tooltip, Input } from 'antd';
 
 // 引入ant中的 +号图标 编辑图标 删除图片 Tooltip->文字提醒功能
-import { PlusOutlined, FormOutlined, DeleteOutlined } from '@ant-design/icons'
+import {
+  PlusOutlined,
+  FormOutlined,
+  DeleteOutlined,
+  CheckOutlined,
+  CloseOutlined
+} from '@ant-design/icons'
 
 // 引入connect  高阶组件 返回容器组件
 import { connect } from 'react-redux'
@@ -19,42 +25,6 @@ import { getSubjectList, getSecSubjectList } from './redux/index'
 // 引入样式文件
 import './index.css'
 
-// 表格列数据
-const columns = [
-  {
-    title: '分类名称',
-    dataIndex: 'title',
-    key: 'title',
-  },
-  {
-    title: '操作',
-    dataIndex: '',
-    key: 'action',
-
-    // 每一列默认展示文本,如果要展示其他内容,使用render属性
-    render: () => (
-      <>
-        {/* 编辑按钮 */}
-        <Tooltip title="点我编辑">
-          <Button type='primary' className='update-btn'>
-            <FormOutlined />
-          </Button>
-        </Tooltip>
-
-        {/* 删除按钮 */}
-        <Tooltip title="点我删除">
-          <Button type='danger'>
-            <DeleteOutlined />
-          </Button>
-        </Tooltip>
-
-      </>
-    ),
-    width: 200
-  }
-]
-
-
 // redux数据状态
 @connect(
   state => ({ subjectList: state.subjectList }),
@@ -62,6 +32,14 @@ const columns = [
 )
 
 class Subject extends Component {
+
+  //定义state中的subjectId数据 用来判断展示title还是input
+  state = {
+    // 有这个subjectId 就是input  没有就是title
+    subjectId: '',
+    subjectTitle: ''
+  }
+
   // 设置默认高亮显示页码
   currentPage = 1
 
@@ -129,7 +107,106 @@ class Subject extends Component {
     }
   }
 
+  // 点击编辑跟新按钮
+  handleUpdateClick = (value) => {
+    // 事件的真正处理函数
+    return (e) => {
+      // 拿到数据
+      // console.log(value)
+      // 修改数据 数据拿到了 也改变了 就可以拿改变的数据进行判断
+      // 是展示title还是input
+      this.setState({
+        subjectId: value._id,
+        subjectTitle: value.title
+      })
+    }
+  }
+
+  // 修改数据时 受控组件input中onchange 的回调函数
+  handleTitleChange = (e) => {
+    this.setState({
+      subjectTitle: e.target.value
+    })
+  }
+
   render () {
+
+    // 表格列数据
+    // 这个column必须要写到render中 因为state变化 render会调用 这个column才会重新执行
+    const columns = [
+      {
+        title: '分类名称',
+        // dataIndex: 'title',    这里不一定是展示title  点击编辑展示input
+        key: 'title',
+        // 当点击 编辑跟新按钮的时候 
+        // 这里就不是展示title了  而是展示input标签
+        // 所以要展示的话 需要在render中展示 title或者input
+        render: (value) => {
+          // render中return什么就展示什么
+          // 这里接收的就是点击编辑按钮得到的对应数据
+          if (this.state.subjectId === value._id) {
+            return <Input
+              className='subject-input'
+              value={this.state.subjectTitle}
+              onChange={this.handleTitleChange}
+            />
+          }
+          return <span>{value.title}</span>
+        }
+      },
+      {
+        title: '操作',
+        dataIndex: '',
+        key: 'action',
+
+        // 每一列默认展示文本,如果要展示其他内容,使用render属性
+        render: (value) => {
+          // 判断当前的id和state中的subjectId是否相同
+          // 相同说明的是点击按钮跳转到了input框
+          // 这个时候要展示确认 取消按钮
+          if (this.state.subjectId === value._id) {
+            // return的是确认 取消按钮
+            return (
+              <>
+                <Button type='primary' className='subject-button'>
+                  <CheckOutlined />
+                </Button>
+                <Button type='danger'>
+                  <CloseOutlined />
+                </Button>
+              </>
+            )
+          }
+
+          // return的是编辑 删除按钮
+          return (
+            <>
+              {/* 编辑按钮 */}
+              <Tooltip title="点我编辑">
+                <Button
+                  type='primary'
+                  className='update-btn'
+                  onClick={this.handleUpdateClick(value)}
+                >
+                  <FormOutlined />
+                </Button>
+              </Tooltip>
+
+              {/* 删除按钮 */}
+              <Tooltip title="点我删除">
+                <Button type='danger'>
+                  <DeleteOutlined />
+                </Button>
+              </Tooltip>
+
+            </>
+          )
+
+        },
+        width: 200
+      }
+    ]
+
     return <div className='subject'>
       <Button
         type="primary"
